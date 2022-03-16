aafd81f5d2657f2286eee634f77119c74ad69d86
Michael Scott <thebluenowhere@protonmail.com>
2022-03-16 09:33:41 +0000

    first exercise complete

---
+++ ex1.py
@@ -0,0 +1,17 @@
+"""
+1. Make your script print another line. 
+2. Make your script print only one of the lines.
+3. Put an octothorpe character at the beginning of a line. What did it do? 
+    - A: It comments out a line of code. 
+
+"""
+
+# print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("I was asked to print another line")
+
