58aebaa2ee1e73541ade24cd4b0eee07552e9eb2
Michael Scott <thebluenowhere@protonmail.com>
2022-03-16 09:58:14 +0000

    ex3 complete

---
+++ ex3.py
@@ -0,0 +1,33 @@
+""" 1. Above each line, use the # to write a comment to yourself explaining what the line does.
+    2. Remember in Ex0 when you started python. Start python again and, using the mat operators, use        python as a calculator. 
+    3. Find something you need to calculate and write a new .py file that does it. 
+    4. Rewrite ex3.py to use floating point numbers so it's more accurate. 
+"""
+
+# prints a string
+print("I will now count my chickens:")
+
+# prints a string and then a quantification seperated by a comma
+print("Hens", 25. + 30. / 6.)
+print("Roosters", 100. - 25. * 3. % 4.)
+
+print("Now I will count the eggs:")
+
+# more maths but with modulus and all other operators
+print(3. + 2. + 1. - 5. + 4. % 2. - 1. / 4. + 6.)
+
+print("Is it true that 3 + 2 < 5 -7?")
+
+print(3. + 2. < 5. - 7.)
+
+print("What is 3 + 2?", 3. + 2.)
+print("What is 5 - 7?", 5. - 7.)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5. > -2.)
+print("Is it greater or equal?", 5. >= -2.)
+print("Is it less or equal?", 5. <= -2.)
+
---
+++ ex3_calculate.py
@@ -0,0 +1,9 @@
+days_in_year = 365
+
+def weeks_in_year():
+    days = 7
+    weeks = days_in_year / days
+    print(f"There are {round(weeks)} in a year.")
+
+weeks_in_year()
+
