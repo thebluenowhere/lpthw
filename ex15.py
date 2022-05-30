f2e1de1eb6caf479a243cf34d11ef19605550f2c
Michael Scott <thebluenowhere@protonmail.com>
2022-05-30 15:55:57 +0100

    ex15

--- ex15.py
+++ ex15.py
@@ -1,12 +1,18 @@
+# import the argument variable module
 from sys import argv
+
+# unpack argv so filename can be entered on terminal
 script, filename = argv
 
+#use the open function on our filename
 txt = open(filename)
 
 print(f"Here's your file {filename}:")
+# use the read function on our file
 print(txt.read())
 
 print("Type the filename again:")
+# repeat or enter a different file
 file_again = input("> ")
 
 txt_again = open(file_again)
