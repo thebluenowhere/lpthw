5402b3f280395fb2883ac0bfd596f7fdf65a3c73
Michael Scott <thebluenowhere@protonmail.com>
2022-05-30 14:59:30 +0100

    s

--- ex13.py
+++ ex13.py
@@ -1,3 +1,4 @@
+"""
 from sys import argv
 
 script, first, second, third = argv
@@ -6,3 +7,15 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+"""
+
+
+from sys import argv
+
+breads, roll, bun = argv
+
+print("The script is called", breads)
+print("The first value is", roll)
+print("The second value is", bun)
+ask = input("What is your favorite bread? ")
+print(f"The users favorite bread is {ask}.")
